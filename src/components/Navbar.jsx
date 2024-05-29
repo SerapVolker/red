4099d3e30a251c531bb828963a852f03e417ff54
SerapVolker <petroveldar926@gmail.com>
2024-05-29 15:15:10 +0300

    rope

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+
+import React from 'react'
+import './App.css'
+const Navbar = () => {
+  return (
+<div className='hair'>
+    
+    <div className='nevbar-hair'>
+     <img src="Rectangle 5.svg" alt="" width="2000px"/>
+     </div> 
+    <div className='logo'>
+     <img src='Group 40.svg'alt=''width="100px"/>
+    </div>
+    <div className='news'>
+    новости
+    </div>
+    <div className='me'>
+        о нас
+    </div>
+    <div className='new'>
+        новинки
+    </div>
+    <div className='contact'>
+        контакты
+    </div>
+    <div className='basket'>
+        <img src="shopping_cart.png" alt="" width='50px'/>
+    </div>
+    <div className='point'>
+        <img src='Ellipse 2.png'alt='' width='30px'/>
+    </div>
+    <div className='one'>
+        1
+    </div>
+    <div className='photo'>
+        <img src='Rectangle 4.png'alt=''width="917px"/>
+    </div>
+    <div className='block'>
+        <img src='Rectangle 2.png'alt=''width="700px"/>
+    </div>
+    <div className='style'>
+    СТИЛЬНАЯ МУЖСКАЯ 
+    <div className='stile'>
+     И ЖЕНСКАЯ ОДЕЖДА
+    </div>
+    </div>
+    <div className='button'>
+        <img src='Rectangle 3.png' alt='' width="400px"/>
+    <div className='text1'>
+    смотреть каталог
+    </div>
+    </div>
+    <div className='basement'>
+        <img src='Rectangle 15.png'alt='' width="2000px"/>
+        <div className='number'>
+        Пн-Вс 10:00-19:00
+    
+    </div>
+    <div className='numb'>
+        +7 903 184-56-70 
+    </div>
+    <div className='man'>
+        Мужчинам 
+    </div>
+    <div className='woman'>
+        Женщинам
+    </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Navbar
